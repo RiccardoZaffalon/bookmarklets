8684e015bb566e9cd9fee4a6db82ebe1777bfec5
Riccardo Zaffalon <riccardo.zaffalon@gmail.com>
2017-11-10 18:23:01 +0100

    Update black_and_white.js

--- black_and_white.js
+++ black_and_white.js
@@ -1,7 +1,9 @@
 (function(){
 	const elements = document.body.querySelectorAll('*');
+	const images = document.getElementsByTagName('img');
 	elements.forEach(el => {
 		el.style.backgroundColor = 'white';
 		el.style.color = 'black';
-	})
+	});
+	images.forEach(img => img.remove());
 })()
